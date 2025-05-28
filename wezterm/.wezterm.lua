036ea38ca509102c957ad78158530d0c130fae38
Zach Goldman <zach.goldman@gusto.com>
2025-05-28 09:46:18 -0700

    Enable opt+arrow in wezterm for moving between words

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -20,19 +20,23 @@ MODIFIER = 'CTRL|SHIFT'
 
 config.keys = {
   -- Font size changing. We'll be using these keys for pane management.
-  { key = '(',         mods = MODIFIER, action = wezterm.action.DecreaseFontSize },
-  { key = ')',         mods = MODIFIER, action = wezterm.action.IncreaseFontSize },
+  { key = '(',          mods = MODIFIER, action = wezterm.action.DecreaseFontSize },
+  { key = ')',          mods = MODIFIER, action = wezterm.action.IncreaseFontSize },
 
   -- Move between panes directionally.
-  { key = 'h',         mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Left'), },
-  { key = 'l',         mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Right'), },
-  { key = 'k',         mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Up'), },
-  { key = 'j',         mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Down'), },
+  { key = 'h',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Left'), },
+  { key = 'l',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Right'), },
+  { key = 'k',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Up'), },
+  { key = 'j',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Down'), },
 
   -- Pane management.
-  { key = '|',         mods = MODIFIER, action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-  { key = '_',         mods = MODIFIER, action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
-  { key = "Backspace", mods = MODIFIER, action = wezterm.action.CloseCurrentPane { confirm = false } },
+  { key = '|',          mods = MODIFIER, action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+  { key = '_',          mods = MODIFIER, action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
+  { key = "Backspace",  mods = MODIFIER, action = wezterm.action.CloseCurrentPane { confirm = false } },
+
+  -- Cursor movement.
+  { key = 'LeftArrow',  mods = 'OPT',    action = wezterm.action.SendString '\x1bb' },
+  { key = 'RightArrow', mods = 'OPT',    action = wezterm.action.SendString '\x1bf' },
 }
 
 return config
