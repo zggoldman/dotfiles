c46d4078e8be3a0a9df7042cd4f067d0b75cd300
Zach Goldman <zggoldman@pm.me>
2025-11-10 11:22:39 -0800

    Moving to tokyo night

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,12 +6,14 @@ local config = wezterm.config_builder()
 config.scrollback_lines = 1000000
 
 -- Font options.
-config.font = wezterm.font('CaskaydiaCove Nerd Font Mono')
-config.font_size = 18.0
+config.font = wezterm.font_with_fallback { 'CaskaydiaMono Nerd Font Mono', 'CaskaydiaCove Nerd Font Mono', 'CaskaydiaMono Nerd Font Mono' }
+config.font_size = 16.0
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable font ligatures.
 
+config.window_background_opacity = 0.9
+
 -- Theming
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tokyo Night"
 
 -- Keybinds.
 config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
@@ -19,24 +21,24 @@ config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
 MODIFIER = 'CTRL|SHIFT'
 
 config.keys = {
-  -- Font size changing. We'll be using these keys for pane management.
-  { key = '(',          mods = MODIFIER, action = wezterm.action.DecreaseFontSize },
-  { key = ')',          mods = MODIFIER, action = wezterm.action.IncreaseFontSize },
+    -- Font size changing. We'll be using these keys for pane management.
+    { key = '(',          mods = MODIFIER, action = wezterm.action.DecreaseFontSize },
+    { key = ')',          mods = MODIFIER, action = wezterm.action.IncreaseFontSize },
 
-  -- Move between panes directionally.
-  { key = 'h',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Left'), },
-  { key = 'l',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Right'), },
-  { key = 'k',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Up'), },
-  { key = 'j',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Down'), },
+    -- Move between panes directionally.
+    { key = 'h',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Left'), },
+    { key = 'l',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Right'), },
+    { key = 'k',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Up'), },
+    { key = 'j',          mods = MODIFIER, action = wezterm.action.ActivatePaneDirection('Down'), },
 
-  -- Pane management.
-  { key = '|',          mods = MODIFIER, action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-  { key = '_',          mods = MODIFIER, action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
-  { key = "Backspace",  mods = MODIFIER, action = wezterm.action.CloseCurrentPane { confirm = false } },
+    -- Pane management.
+    { key = '|',          mods = MODIFIER, action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = '_',          mods = MODIFIER, action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
+    { key = "Backspace",  mods = MODIFIER, action = wezterm.action.CloseCurrentPane { confirm = false } },
 
-  -- Cursor movement.
-  { key = 'LeftArrow',  mods = 'OPT',    action = wezterm.action.SendString '\x1bb' },
-  { key = 'RightArrow', mods = 'OPT',    action = wezterm.action.SendString '\x1bf' },
+    -- Cursor movement.
+    { key = 'LeftArrow',  mods = 'OPT',    action = wezterm.action.SendString '\x1bb' },
+    { key = 'RightArrow', mods = 'OPT',    action = wezterm.action.SendString '\x1bf' },
 }
 
 return config
